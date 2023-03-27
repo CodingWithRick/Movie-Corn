6ba207ad2a07c2ad5c28defd50483fc2c2613228
CodingWithRick <nitubasukundu@gmail.com>
2023-03-27 13:38:03 +0530

    Bug Fixed

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function showMovie(data){
         movieEl.innerHTML = `<img src="${IMG_URL + poster_path}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
-            <span class="${getColor(vote_average)}">${vote_average}</span>
+            <span class="${getColor(vote_average.toFixed(2))}">${vote_average.toFixed(2)}</span>
         </div>
         <div class="overview">
             <h3>Overview</h3>
